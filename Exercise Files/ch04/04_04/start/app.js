b356fe621399baee19d6bce694110bd4933a3547
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-14 23:57:10 +0500

    04_04 imported funciton destructured

--- Exercise Files/ch04/04_04/start/app.js
+++ Exercise Files/ch04/04_04/start/app.js
@@ -1,7 +1,8 @@
-const counter = require("./myModule");
+const { inc, dec, getCount } = require("./myModule");
 
-counter.inc();
-counter.inc();
-counter.inc();
+inc();
+inc();
+inc();
+dec();
 
-console.log(counter.getCount());
+console.log(getCount());
