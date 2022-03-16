eac8be903a08540595ce3e712af17a5e2d18f542
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-16 08:17:17 +0500

    04_07 improve a module with event emitter

--- Exercise Files/ch04/04_07/start/questions.js
+++ Exercise Files/ch04/04_07/start/questions.js
@@ -3,11 +3,23 @@ const collectAnswers = require("./lib/collectAnswers");
 const questions = [
   "What is your name? ",
   "Where do you live? ",
-  "What are you going to do with node js? "
+  "What are you going to do with node js? ",
 ];
 
-collectAnswers(questions, answers => {
+const answerEvents = collectAnswers(questions, (answers) => {
   console.log("Thank you for your answers. ");
   console.log(answers);
+  process.exit();
+});
+
+answerEvents.on("answer", (answers) => {
+  console.log("Thank you for your answers.", answers);
+});
+
+answerEvents.on("complete", (answers) => {
+  console.log("Thank you for your answers.", answers);
+});
+
+answerEvents.on("complete", () => {
   process.exit();
 });
