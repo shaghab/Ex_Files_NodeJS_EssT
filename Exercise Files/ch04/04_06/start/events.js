9033b93b19e9cdc21d34940aa5d3045db3d57a77
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-16 08:02:53 +0500

    04_06 Customer events with the EventEmitter part 2

--- Exercise Files/ch04/04_06/start/events.js
+++ Exercise Files/ch04/04_06/start/events.js
@@ -6,5 +6,14 @@ emitter.on("customEvent", (message, user) => {
   console.log(`${user}: ${message}`);
 });
 
-emitter.emit("customEvent", "Hello World", "Computer");
-emitter.emit("customEvent", "That's pretty cool huh?", "Alex");
+// emitter.emit("customEvent", "Hello World", "Computer");
+// emitter.emit("customEvent", "That's pretty cool huh?", "Alex");
+
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+  if (input === "exit") {
+    emitter.emit("customEvent", "Googebye!", "process");
+    process.exit();
+  }
+  emitter.emit("customEvent", input, "terminal");
+});
