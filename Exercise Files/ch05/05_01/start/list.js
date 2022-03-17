5ff2f8a0847d51e1a31936f12af24702514778c4
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-17 23:43:30 +0500

    05_01 List Directory file asynchronous method

--- Exercise Files/ch05/05_01/start/list.js
+++ Exercise Files/ch05/05_01/start/list.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 
+fs.readdir("./assets", (err, files) => {
+  if (err) {
+    throw err;
+  }
+  console.log("complete");
+  console.log(files);
+});
 console.log("started reading files");
-const files = fs.readdirSync("./assets");
-console.log("complete");
-
-console.log(files);
