958bc15cbe47f76909193a2361137760a5cc3903
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-17 23:55:13 +0500

    05_02 Read Files - Error Handling

--- Exercise Files/ch05/05_02/start/readFile.js
+++ Exercise Files/ch05/05_02/start/readFile.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 
-fs.readFile("./assets/alex.jpg", (err, img) => {
+fs.readFile("./assets/alex.jpeg", (err, img) => {
+  if (err) {
+    console.log(`An error has occured: ${err.message}`);
+    process.exit();
+  }
   console.log("file content read");
   console.log(img);
 });
