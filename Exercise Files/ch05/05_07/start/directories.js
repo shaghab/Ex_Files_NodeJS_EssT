58689b9c04628d8c3435636c4b2fc5c56b938cf8
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-19 21:34:20 +0500

    05_07 compelted

--- Exercise Files/ch05/05_07/start/directories.js
+++ Exercise Files/ch05/05_07/start/directories.js
@@ -1,3 +1,13 @@
 const fs = require("fs");
 
-fs.renameSync("./storage-files", "./storage");
+fs.readdirSync("./storage").forEach((fileName) => {
+  fs.unlinkSync(`./storage/${fileName}`);
+});
+
+fs.rmdir("./storage", (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log("./storage director removed");
+});
