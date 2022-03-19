f9b037104baf4d383788c5a3362dd0932696a8ec
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-19 22:03:15 +0500

    06_02 Writable file streams - process.stdin changed with readstream

--- Exercise Files/ch06/06_02/start/writeStream.js
+++ Exercise Files/ch06/06_02/start/writeStream.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 const writeStream = fs.createWriteStream("./assets/myFile.txt", "UTF-8");
 
-process.stdin.on("data", (data) => {
+const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
+
+readStream.on("data", (data) => {
   writeStream.write(data);
 });
