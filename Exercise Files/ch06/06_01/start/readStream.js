e1f308ec824d55cf9fa4bd5717f12462ec83e2d4
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-19 21:48:32 +0500

    06_01 Readable file streams completed

--- Exercise Files/ch06/06_01/start/readStream.js
+++ Exercise Files/ch06/06_01/start/readStream.js
@@ -1,4 +1,20 @@
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
+
+let fileTxt = "";
+
 console.log("type something...");
-process.stdin.on("data", (data) => {
+readStream.on("data", (data) => {
   console.log(`I read ${data.length - 1} characters of text`);
+  fileTxt += data;
+});
+
+readStream.once("data", (data) => {
+  console.log(data);
+});
+
+readStream.on("end", () => {
+  console.log("finished reading file");
+  console.log(`In total i read ${fileTxt.length - 1} characters of text`);
 });
