5f13f9d2526d953d9ffd472bb9f63bb0da2c4061
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-19 22:06:09 +0500

    06_02 Writeable file stream - piping streams

--- Exercise Files/ch06/06_02/start/writeStream.js
+++ Exercise Files/ch06/06_02/start/writeStream.js
@@ -4,6 +4,4 @@ const writeStream = fs.createWriteStream("./assets/myFile.txt", "UTF-8");
 
 const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
 
-readStream.on("data", (data) => {
-  writeStream.write(data);
-});
+process.stdin.pipe(writeStream);
