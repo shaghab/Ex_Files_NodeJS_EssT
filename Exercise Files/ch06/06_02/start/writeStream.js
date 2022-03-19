742cfc47f1008f371d04076a7ff7c9a836ff6585
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-19 22:07:35 +0500

    06_02 writebale file streams - swapping process.stdout with readStream

--- Exercise Files/ch06/06_02/start/writeStream.js
+++ Exercise Files/ch06/06_02/start/writeStream.js
@@ -4,4 +4,4 @@ const writeStream = fs.createWriteStream("./assets/myFile.txt", "UTF-8");
 
 const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
 
-process.stdin.pipe(writeStream);
+readStream.pipe(writeStream);
