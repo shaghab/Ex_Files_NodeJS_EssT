b4d1967114333a43798f8dbee1e9ba053ae06b96
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-03-19 21:57:43 +0500

    06_02 Write file stream - make read stream work with write streams

--- Exercise Files/ch06/06_02/start/writeStream.js
+++ Exercise Files/ch06/06_02/start/writeStream.js
@@ -2,5 +2,6 @@ const fs = require("fs");
 
 const writeStream = fs.createWriteStream("./assets/myFile.txt", "UTF-8");
 
-writeStream.write("hello");
-writeStream.write(" world\n");
+process.stdin.on("data", (data) => {
+  writeStream.write(data);
+});
